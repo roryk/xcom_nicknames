67657bea06053007d5e6bce30d0af18a9f6ea771
roryk <rory.kirchner@gmail.com>
2012-11-14 01:50:31 -0500

    Added generating first names.

--- xcom_nicknames.py
+++ xcom_nicknames.py
@@ -2,10 +2,20 @@ from random import choice
 import sys
 
 ROLES = ["Support", "Heavy", "Assault", "Sniper"]
+COUNTRIES = ["Am", "Rs", "Ch", "In", "Af", "Mx", "Ab",
+             "En", "Fr", "Gm", "Au", "It", "Jp",
+             "Is", "Es", "Gr", "Nw", "Ir", "Sk",
+             "Du", "Sc", "Bg"]
+
+
+def name2string(gender, nickname, country):
+    firstname = "m_arr%s%sFirstNames=%s\n" % (country, gender.upper(),
+                                              nickname)
+    lastname = "m_arr%sLastNames= \n" % (country)
+    return (firstname, lastname)
 
 
 def nickname2string(gender, nickname, role, number):
-
     nstring = "m_arr%s%sNicknames[%s]=%s\n" % (gender.upper(), role,
                                              str(number), nickname)
     return nstring
@@ -19,6 +29,12 @@ def read_nicknames(in_file):
             nicknames[gender].append(line.split("=")[1].strip())
     return nicknames
 
+def output_names(in_file):
+    with open(in_file) as in_handle:
+        for line in in_handle:
+            for country in COUNTRIES:
+                names = name2string(line[0], line.split("=")[1].strip(), country)
+                map(sys.stdout.write, names)
 
 def output_nicknames(nicknames):
     for role in ROLES:
@@ -30,9 +46,22 @@ def output_nicknames(nicknames):
                 count += 1
 
 
-def main(in_file):
-    nicknames = read_nicknames(in_file)
-    output_nicknames(nicknames)
+def main(in_file, to_generate):
+    if to_generate == "nickname":
+        nicknames = read_nicknames(in_file)
+        output_nicknames(nicknames)
+    elif to_generate == "first":
+        output_names(in_file)
+    else:
+        print usage
+        exit(1)
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    usage = """usage: python xcom_nicknames.py nicknames_file nickname/first
+    adding nickname will output nicknames. first will output as first names
+    """
+    if len(sys.argv) < 3:
+        print usage
+        exit(1)
+
+    main(sys.argv[1], sys.argv[2])
